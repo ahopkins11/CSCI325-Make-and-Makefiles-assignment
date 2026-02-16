d69c8f7585a569549dce9e9334581c216e01075f
ahopkins11 <aj.hopkins2011@me.com>
2026-02-15 20:45:07 -0600

    vector.h

---
+++ HW1/Vector.h/vector.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector {
+ private:
+  int *Vec_ptr;
+  int Vec_size;
+  int Vec_capacity;
+
+ public:
+  Vector();
+  Vector(const Vector &other);
+  ~Vector();
+
+  Vector& operator=(const Vector &other);
+
+  int size();
+  int capacity();
+  void push_back(int element);
+  void reserve(int n);
+  int& operator[](unsigned int index);
+};
+
+#endif
